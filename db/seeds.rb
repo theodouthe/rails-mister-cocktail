2d7c908dca1242af43fd2d17cff004308bb45fa3
Theophane Douthe <theophanedouthe@yahoo.fr>
2018-08-09 12:24:21 +0200

    ingredients seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "Cleaning the db..."
+Ingredient.destroy_all
+
+
+puts 'Creating ingredients...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks = open(url).read
+ingredient = JSON.parse(drinks)
+ingredient["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+  end
+
+puts 'Finished!'
