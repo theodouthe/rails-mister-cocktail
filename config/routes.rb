e690f7a4d8c7d8e3b9f435bca204e648776be3ff
Theophane Douthe <theophanedouthe@yahoo.fr>
2018-08-10 16:40:43 +0200

    root added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails  do
+  root "cocktails#index"
+  resources :cocktails, only: [:show, :new, :create, :edit, :update, :destroy]  do
     resources :doses, only: [:new, :create, :destroy], shallow: true
   end
 end
